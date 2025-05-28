c5ccbb7144045b3cbb8b89f9495837795b6d73aa
ZakaTiwana <zakatiwana@gmail.com>
2025-05-28 15:27:11 +0500

    improved log tests by adding sut

--- test/log_test.dart
+++ test/log_test.dart
@@ -1,51 +1,52 @@
+import 'package:network_request/network_request.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
-import 'main.dart';
+import 'network_manger/test_manager.dart';
 
 void main() {
   group('[All log tests]', _allLogTests);
 }
 
 void _allLogTests() {
+  NetworkRequest sut() {
+    return TestNetworkManager();
+  }
+
   test('Log Formatted Json test - on 1000 characters', () {
+    final network = sut();
     Map<String, dynamic> json = {
       'test': '1' * (500 - 8 - 6),
       'test2': '2' * (500 - 8 - 7)
     };
     final result = network.logFormattedJson(json);
-    if (network.trimJsonLogs) {
-      expect(false, result.contains('\n...\n'));
-    }
+    expect(false, result.contains('\n...\n'));
   });
 
   test('Log Formatted Json test - on 2000 characters', () {
+    final network = sut();
     Map<String, dynamic> json = {
       'test': '1' * (1000 - 8 - 6),
       'test2': '2' * (1000 - 8 - 7)
     };
     final result = network.logFormattedJson(json);
-    if (network.trimJsonLogs) {
-      expect(false, result.contains('\n...\n'));
-    }
+    expect(false, result.contains('\n...\n'));
   });
 
   test('Log Formatted Json test - on 2001 characters', () {
+    final network = sut();
     Map<String, dynamic> json = {
       'test': '1' * (1000 - 8 - 6),
       'test2': '2' * (1000 - 8 - 6)
     };
     final result = network.logFormattedJson(json);
-    if (network.trimJsonLogs) {
-      expect(true, result.contains('\n...\n'));
-    }
+    expect(true, result.contains('\n...\n'));
   });
 
   test('Log Formatted Json test - on > 2001 characters', () {
+    final network = sut();
     Map<String, dynamic> json = {'test': '1' * 3021, 'test2': '2' * 1300};
     final result = network.logFormattedJson(json);
-    if (network.trimJsonLogs) {
-      expect(true, result.contains('\n...\n'));
-    }
+    expect(true, result.contains('\n...\n'));
   });
 }
